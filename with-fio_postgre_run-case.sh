730e823eeb6484a9a9f7daec34cf3c8b1ba8b88c
cliicy <cliicy@gmail.com>
2019-11-10 07:20:10 -0800

    update vanda

--- with-fio_postgre_run-case.sh
+++ with-fio_postgre_run-case.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-./1_prep_dev.sh ./sysbench-cfg/benchmark.cfg 
+sed -i s/fillfact=75/fillfact=100/ ./sysbench-cfg/benchmark.cfg
+./fulldisk_precond_1_prep_dev.sh ./sysbench-cfg/benchmark.cfg 
 
 pushd fio
-sh ./postgre_precond.sh
+sh ./fulldisk_precond.sh
 rm -rf /opt/data/vanda/fio/*
 popd
 
 ./2_initdb.sh   ./sysbench-cfg/benchmark.cfg
-#./onlyrun_2_initdb.sh   ./sysbench-cfg/benchmark.cfg
 ./3_run.sh      ./sysbench-cfg/benchmark.cfg
 
-#sh dointel.sh
+./do2runwkloads.sh ./sysbench-cfg/benchmark.cfg
+
+
+####fillfact75
+sleep 10
+sed -i s/fillfact=100/fillfact=75/ ./sysbench-cfg/benchmark.cfg
+./fulldisk_precond_1_prep_dev.sh ./sysbench-cfg/benchmark.cfg
+./2_initdb.sh   ./sysbench-cfg/benchmark.cfg
+./3_run.sh      ./sysbench-cfg/benchmark.cfg
+
+./do2runwkloads.sh ./sysbench-cfg/benchmark.cfg
+
+sh dointel_fulldisk.sh
