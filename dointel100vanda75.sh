c8262063cf47031393d47c5e1cf8b2babc3bd6ab
cliicy <cliicy@gmail.com>
2019-11-11 01:32:08 -0800

    update dointel2TB

---
+++ dointel100vanda75.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+sh dointel_fulldisk.sh
+
+sed -i s/fillfact=100/fillfact=75/ ./sysbench-cfg/benchmark.cfg
+./fulldisk_precond_1_prep_dev.sh ./sysbench-cfg/benchmark.cfg 
+
+pushd fio
+sh ./fulldisk_precond.sh
+rm -rf /opt/data/vanda/fio/*
+popd
+
+./2_initdb.sh   ./sysbench-cfg/benchmark.cfg
+./3_run.sh      ./sysbench-cfg/benchmark.cfg
+
+./do2runwkloads.sh ./sysbench-cfg/benchmark.cfg
+
+sh doTPCC.sh
